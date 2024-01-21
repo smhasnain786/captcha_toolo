d461ab38e8bbd8c603856b8066538cebb330d99f
syed <syedhasnain7456@gmail.com>
2024-01-21 15:55:13 +0530

    first change

---
+++ captchaLogic.js
@@ -0,0 +1,86 @@
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+const { log } = require("console");
+require("dotenv").config();
+let b1 = false
+let b2 = false
+let b3 = false
+const captchaLogic = async (res, url) => {
+  console.log(url);
+  const address=url
+  
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+      '--disable-gpu',
+      '--remote-debugging-port=9222'
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com.my/imghp');
+    console.log('Google Image Search page loaded');
+    const button = await page.waitForSelector('div.dRYYxd > div.nDcEnd');
+    console.log(button);
+    await button.click();
+    console.log('Button clicked1');
+    await button.click();
+    await button.click();
+    await button.click();
+    console.log('Button clicked2');
+    console.log(url);
+    await page.$eval('.cB9M7',( el , address )=> el.value = address , address);
+    setTimeout(async () => {
+      const submit = await page.waitForSelector('div.Qwbd3');
+      console.log('----------->', submit);
+      await submit.click();
+    }, 3000)
+    await page.waitForNavigation();
+    const textButton = await page.waitForSelector('#ucj-3');
+    console.log('<---------,', textButton);
+    await textButton.click()
+    await page.waitForSelector('.QeOavc')
+    let element = await page.waitForSelector('[dir="ltr"]')
+    // const values = await page.evaluate(el => el.querySelector('[dir="ltr"]').innerHTMl, element)
+    // console.log((values));
+    const text = await page.evaluate(el => {
+      const spanElement = el.querySelector('div.QeOavc > [dir="ltr"]');
+      b2 = true
+      return spanElement.textContent;
+    }, element);
+    
+    console.log(text);
+
+    setTimeout(async () => {
+  
+      if (text.length > 4) {
+        browser.close();
+        res.send({ captcha: text })
+      } else {
+        browser.close();
+        res.send({URL: url,  captcha: "captcha is unreadable" })
+      }
+    }, 5000)
+
+
+  } catch (e) {
+    console.error(e);
+    browser.close();
+
+    // scrapeLogic(res)
+    res.send(`Something went wrong while running: ${e}`);
+  } finally {
+    // await browser.close();
+  }
+};
+
+module.exports = { captchaLogic };
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { captchaLogic } = require("./captchaLogic");
+const cors = require('cors')
+const path = require('path');
+const app = express();
+// app.use('/images', express.static(path.join(__dirname, 'uploads1')));
+const corsOptions = {
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 4000;
+console.log(path.join(__dirname, ''));
+app.get("/urls/", (req, res) => {
+  captchaLogic(res,req.query.url);
+});
+app.get("/img_name/:add", (req, res) => {
+  captchaLogic(res,`https://client2024.brandanalysisindia.com/public/temp/${req.params.add}`);
+});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
