3ceee17010d38489b51e71f085a17702c92022bb
smhasnain786 <135857084+smhasnain786@users.noreply.github.com>
2024-03-02 17:16:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const express = require("express");
-const { captchaLogic } = require("./captchaLogic");
-const cors = require('cors')
-const path = require('path');
+const express = require('express');
 const app = express();
-// app.use('/images', express.static(path.join(__dirname, 'uploads1')));
+const cors = require('cors')
+const { Cluster } = require('puppeteer-cluster');
 app.use(cors({
   origin: '*',
 }));
-const PORT = process.env.PORT || 4000;
-console.log(path.join(__dirname, ''));
-app.get("/urls/", (req, res) => {
-  captchaLogic(res,req.query.url);
-});
-app.get("/img_name/:add", (req, res) => {
-  captchaLogic(res,`https://client2024.brandanalysisindia.com/public/temp/${req.params.add}`);
-});
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+(async () => {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 5,
+    puppeteerOptions: {
+      headless: false
+    },
+    debug: true,
+  });
+  await cluster.task(async ({ page, data: url }) => {
+    // console.log(url);
+    
+    const address = url
+    await page.goto(`https://lens.google.com/uploadbyurl?url=${address}&ep=cntpubu&hl=en-US&st=1708596832037&cd=CKG1yQEIlbbJAQijtskBCKmdygEI0qDKAQi0kMsBCJOhywEIhaDNAQji7M0BCMzuzQEItPXNAQjs980BCM_8zQEY9cnNARic-M0BGMr4zQE&re=df&s=4`);
+    const textButton = await page.waitForSelector('#ucj-3', { timeout: 5000 });
+    await textButton.click()
+    await page.waitForSelector('.QeOavc', { timeout: 5000 })
+    let element = await page.waitForSelector('[dir="ltr"]')
+    const text = await page.evaluate(el => {
+      const spanElement = el.querySelector('div.QeOavc > [dir="ltr"]');
+      return spanElement.textContent;
+    }, element);
+
+    console.log(text);
+    return text
+
+  });
+
+  // setup server
+  app.get('/', async function (req, res) {
+    if (!req.query.url) {
+      return res.end('Please specify url like this: ?url=example.com');
+    }
+    try {
+      const screen = await cluster.execute(req.query.url);
+      console.log(">>>>>>>>>>>>>", screen);
+      res.json({ url: req.query.url, screen: screen});
+    } catch (err) {
+      res.json({ url: req.query.url, screen: "cant solve", err: err.message });
+    }
+  });
+
+  app.listen(3000, function () {
+    console.log('Screenshot server listening on port 3000.');
+  });
+})();
